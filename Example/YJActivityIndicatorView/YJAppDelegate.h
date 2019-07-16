393e5f4bb758841209f1fd39079fd6e6ed6a4753
刘亚军 <liuyajun1999@icloud.com>
2019-07-16 11:37:31 +0800

    Initial commit

---
+++ Example/YJActivityIndicatorView/YJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJAppDelegate.h
+//  YJActivityIndicatorView
+//
+//  Created by lyj on 07/16/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
